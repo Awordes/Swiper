75e6f160e4a528bbafe27af029c9a180c273caab
Andrew Tolstov <awordes@yandex.ru>
2023-12-16 00:55:12 +0300

    Added SwipeUp test

---
+++ Swiper.Tests/SwiperSimulatorTests.cs
@@ -0,0 +1,43 @@
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using Swiper.Simulator;
+using Swiper.Simulator.API;
+
+namespace Swiper.Tests;
+
+public class SwiperSimulatorTests
+{
+    [Fact]
+    public void SwipeUp_ShouldSwipeUp()
+    {
+        var serviceProvider = BuildServiceProvider();
+
+        var simulatorService = serviceProvider.GetRequiredService<ISimulatorService>();
+
+        var size = 4;
+        var input = new int[size][];
+        input[0] = [4, 0, 2, 2];
+        input[1] = [0, 2, 4, 2];
+        input[2] = [4, 2, 2, 4];
+        input[3] = [2, 4, 2, 4];
+
+        var expect = new int[size][];        
+        expect[0] = [8, 4, 2, 4];
+        expect[1] = [2, 4, 4, 8];
+        expect[2] = [0, 0, 4, 0];
+        expect[3] = [0, 0, 0, 0];
+
+        simulatorService.StartSimulation(size, input);
+        simulatorService.SwipeUp();
+        var result = simulatorService.GetMatrix();
+        
+        Assert.Equal(expect, result);
+    }
+
+    private static ServiceProvider BuildServiceProvider()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddSimulator();
+        return serviceCollection.BuildServiceProvider();
+    } 
+}
