0ee9193d98ee035fd924a529ea1299eeb95c64fb
Andrew Tolstov <awordes@yandex.ru>
2023-12-16 01:29:51 +0300

    Added startup project

---
+++ Swiper.Startup/Program.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Swiper.Simulator;
+using Swiper.Simulator.API;
+
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddSimulator();
+var serviceProvider = serviceCollection.BuildServiceProvider();
+var simulatorService = serviceProvider.GetRequiredService<ISimulatorService>();
