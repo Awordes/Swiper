3e5ed799942136a89ff3ca7bf0c11564fdc5605c
Andrew Tolstov <awordes@yandex.ru>
2023-12-16 01:27:23 +0300

    Added complex test

--- Swiper.Tests/SwiperSimulatorTests.cs
+++ Swiper.Tests/SwiperSimulatorTests.cs
@@ -114,6 +114,37 @@ public class SwiperSimulatorTests
         
         Assert.Equal(expect, result);
     }
+    
+    [Fact]
+    public void AllSwipes_ShouldSwipeCorrect()
+    {
+        var serviceProvider = BuildServiceProvider();
+
+        var simulatorService = serviceProvider.GetRequiredService<ISimulatorService>();
+
+        var size = 4;
+        var input = new int[size][];
+        input[0] = [4, 0, 2, 2];
+        input[1] = [0, 2, 4, 2];
+        input[2] = [4, 2, 2, 4];
+        input[3] = [2, 4, 2, 4];
+
+        var expect = new int[size][];
+        expect[0] = [8, 4, 16, 4];
+        expect[1] = [8, 0, 0, 0];
+        expect[2] = [0, 0, 0, 0];
+        expect[3] = [0, 0, 0, 0];
+
+        simulatorService.StartSimulation(size, input);
+        simulatorService.SwipeUp();
+        simulatorService.SwipeRight();
+        simulatorService.SwipeDown();
+        simulatorService.SwipeLeft();
+        simulatorService.SwipeUp();
+        var result = simulatorService.GetMatrix();
+        
+        Assert.Equal(expect, result);
+    }
 
     private static ServiceProvider BuildServiceProvider()
     {
